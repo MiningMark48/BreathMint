9146256d827aa19ed626bd0880734a0cb0f614dd
Mark <miningmark48@gmail.com>
2019-11-02 17:33:18 -0400

    Init

---
+++ src/main/java/com/miningmark48/breathmint/BreathMint.java
@@ -0,0 +1,55 @@
+package com.miningmark48.breathmint;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("breathmint")
+public class BreathMint {
+
+    private static BreathMint instance = null;
+
+    public static BreathMint getInstance() {
+        assert instance != null;
+        return instance;
+    }
+
+    public BreathMint() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+//        ModLoadingContext.get().registerConfig(Type.SERVER, ModConfig.serverSpec);
+//        ModLoadingContext.get().registerConfig(Type.CLIENT, ModConfig.clientSpec);
+//        ModLoadingContext.get().registerConfig(Type.COMMON, ModConfig.commonSpec);
+
+        eventBus.addListener(this::setup);
+        eventBus.addListener(this::finishLoad);
+        eventBus.addListener(this::setupClient);
+
+//        eventBus.addListener(ModConfig::onLoad);
+//        eventBus.addListener(ModConfig::onFileChange);
+
+        MinecraftForge.EVENT_BUS.register(new EventPlayerInteract());
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+
+    private void setup(FMLCommonSetupEvent event) {
+        instance = (BreathMint) ModLoadingContext.get().getActiveContainer().getMod();
+    }
+
+    private void setupClient(final FMLClientSetupEvent event) {
+
+    }
+
+    private void finishLoad(FMLLoadCompleteEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/miningmark48/breathmint/EventPlayerInteract.java
@@ -0,0 +1,27 @@
+package com.miningmark48.breathmint;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class EventPlayerInteract {
+
+    @SubscribeEvent
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        BlockState clickedState = event.getWorld().getBlockState(event.getPos());
+        if (clickedState.getBlock() == Blocks.DRAGON_HEAD && event.getWorld().isBlockPowered(event.getPos()) && event.getItemStack().getItem() == Items.GLASS_BOTTLE) {
+            ItemEntity itemEntity = new ItemEntity(event.getWorld(), event.getPlayer().posX, event.getPlayer().posY, event.getPlayer().posZ);
+            itemEntity.setItem(new ItemStack(Items.DRAGON_BREATH));
+            event.getWorld().addEntity(itemEntity);
+            event.getItemStack().shrink(1);
+//            event.getPlayer().sendStatusMessage(new StringTextComponent("CLICKED HEAD"), false);
+        }
+    }
+
+}
