5bb4d967181170990be52c7a3335be431a2f799e
Mark <miningmark48@gmail.com>
2019-11-05 18:11:50 -0500

    Bug fix with wall dragons

--- src/main/java/com/miningmark48/breathmint/EventInteract.java
+++ src/main/java/com/miningmark48/breathmint/EventInteract.java
@@ -10,12 +10,12 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
-public class EventPlayerInteract {
+public class EventInteract {
 
     @SubscribeEvent
     public void onPlayerInteract(PlayerInteractEvent event) {
         BlockState clickedState = event.getWorld().getBlockState(event.getPos());
-        if (clickedState.getBlock() == Blocks.DRAGON_HEAD && event.getWorld().isBlockPowered(event.getPos()) && event.getItemStack().getItem() == Items.GLASS_BOTTLE) {
+        if ((clickedState.getBlock() == Blocks.DRAGON_HEAD || clickedState.getBlock() == Blocks.DRAGON_WALL_HEAD) && event.getWorld().isBlockPowered(event.getPos()) && event.getItemStack().getItem() == Items.GLASS_BOTTLE) {
             ItemEntity itemEntity = new ItemEntity(event.getWorld(), event.getPlayer().posX, event.getPlayer().posY, event.getPlayer().posZ);
             itemEntity.setItem(new ItemStack(Items.DRAGON_BREATH));
             event.getWorld().addEntity(itemEntity);
@@ -23,5 +23,7 @@ public class EventPlayerInteract {
 //            event.getPlayer().sendStatusMessage(new StringTextComponent("CLICKED HEAD"), false);
         }
     }
+
+//    public void onDispenser()
 
 }
